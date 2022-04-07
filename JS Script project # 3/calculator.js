5b31479d4b879292bb6a185a57ac7cd5dfaaf713
AnthonyLaneJr <101611103+AnthonyLaneJr@users.noreply.github.com>
2022-04-06 20:22:55 -0700

    Class 103-Assignment # 3
    A simple calculator using 2 numbers via JavaScript.

--- JS Script project # 3/calculator.js
+++ JS Script project # 3/calculator.js
@@ -1,6 +1,6 @@
 function calculator(){
     console.log("Calculating...");
-    let num1 = Number(prompt("Enter the number"));//parse into a number
-
-    document.getElementById(`calculationZone`).innerHTML=`<p>${num1} * 10 = ${num1*10}</p>`;
+    let num1 = Number(prompt("Enter the number"));
+    let num2 = Number(prompt("Enther the second number"));
+    document.getElementById(`calculationZone`).innerHTML=`<div class="number-row"><p>First Number: ${num1}</p><p>Second Number: ${num2}</p></div><div class=row-container><p>Addition: ${num1+num2}</p><p>Subtraction: ${num1-num2}</p><p>Multiplication: ${num1*num2}</p><p>Division: ${num1/num2}</p></div>`;
 }
